7fa8e7938c828ce0aa73e1ae5bcde7d16b668d14
unknown <luojs@al.com>
2023-08-11 09:54:18 +0800

    新增添的文件

---
+++ 1.js
@@ -0,0 +1,426 @@
+window=global;
+location={
+    href:'https://www.koreanair.com/booking/select-flight/departure'
+}
+var document={
+    "URL":'https://www.koreanair.com/booking/select-flight/departure',
+    "abck_old_cookie":'99D882422C9A970CB9EC64BA1B8E0EF6~0~YAAQD4yUGzbQKyKCAQAA4jAQVwg8pEwjmxZiqx68t6jfyAxBdlJiF4lDVkJi0L00iSyxxNmv9cyF1BMFfEfn5/bSV2MzWCJU9DcbuSFkHZnJf128HCUzKVGV9c65rF2ctwHu8d2UYRaa6ITvgPm854bclqbafAEchY/7vvsedsikk4/LLGXHxJA7thjcNZ6qARL2Nk+bTnZuXiblixm/ayhFBqMF+ubaiQ751KSjbnbr2BJdSUjwaydpYYoJ8P8fmKuK074QY6g8NNm59d36+NilLTw8Vou5n6LbIg/gLr1DfANdVCssuiL2kllN+CLrJn1EDfUWGGqqi17gKJfcLTgeUPSw9KBceBgvGge1VQ4QlBX/11jxuYExXBDBHggqR1y+7aNTZmSe4Se3sgeDl+b6cIkW4HXt9CQQ~-1~-1~-1',
+
+}
+var navigator={
+    "pua":"Mozilla/5.0 (Windows;14.0.0;x86;64;) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36",
+    "userAgent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+    "productSub": "20030107",
+    "language": 'zh-CN',
+    "product": 'Gecko',
+    "plugins": {0: {},1: {},2: {},3: {}}
+}
+var screen = {
+    "availHeight": 1104,
+    "availWidth": 2048,
+    "width": 2048,
+    "height": 1152
+}
+window["navigator"]=navigator
+window["document"]=document
+
+window["screen"] = screen;
+window["innerHeight"] = 1002;
+window["innerWidth"] = 2048;
+window["outerWidth"] = 160;
+
+var  bmak = {
+    ver: 1.75,
+    ke_cnt_lmt: 150,
+    mme_cnt_lmt: 100,
+    mduce_cnt_lmt: 75,
+    pme_cnt_lmt: 25,
+    pduce_cnt_lmt: 25,
+    tme_cnt_lmt: 25,
+    tduce_cnt_lmt: 25,
+    doe_cnt_lmt: 10,
+    dme_cnt_lmt: 10,
+    vc_cnt_lmt: 100,
+    doa_throttle: 0,
+    dma_throttle: 0,
+    session_id: "default_session",
+    js_post: !1,
+    loc: "",
+    cf_url: "https://",
+    params_url: 'https://www.koreanair.com/get_params',
+    auth: "",
+    api_public_key: "afSbep8yjnZUjq3aL010jO15Sawj2VZfdYK8uY90uxq",
+    aj_lmt_doact: 1,
+    aj_lmt_dmact: 1,
+    aj_lmt_tact: 1,
+    ce_js_post: 0,
+    init_time: 0,
+    informinfo: "",
+    prevfid: -1,
+    fidcnt: 0,
+    sensor_data: 0,
+    pua: "",
+    ins: null,
+    cns: null,
+    enGetLoc: 0,
+    enReadDocUrl: 1,
+    disFpCalOnTimeout: 0,
+    xagg: -1,
+    pen: -1,
+    brow: "",
+    browver: "",
+    psub: "-",
+    lang: "-",
+    prod: "-",
+    plen: -1,
+    doadma_en: 0,
+    sdfn: [],
+    d2: 0,
+    d3: 0,
+    thr: 0,
+    cs: "0a46G5m17Vrp4o4c",
+    hn: "unk",
+    z1: 0,
+    o9: 0,
+    vc: "",
+    y1: 2016,
+    ta: 0,
+    tst: -1,
+    t_tst: 0,
+    ckie: "_abck",
+    n_ck: "0",
+    ckurl: 0,
+    bm: !1,
+    mr: "-1",
+    altFonts: !1,
+    rst: !1,
+    runFonts: !1,
+    fsp: !1,
+    firstLoad: !0,
+    pstate: !1,
+    mn_mc_lmt: 10,
+    mn_state: 0,
+    mn_mc_indx: 0,
+    mn_sen: 0,
+    mn_tout: 100,
+    mn_stout: 1e3,
+    mn_ct: 1,
+    mn_cc: "",
+    mn_cd: 1e4,
+    mn_lc: [],
+    mn_ld: [],
+    mn_lcl: 0,
+    mn_al: [],
+    mn_il: [],
+    mn_tcl: [],
+    mn_r: [],
+    mn_rt: 0,
+    mn_wt: 0,
+    mn_abck: "",
+    mn_psn: "",
+    mn_ts: "",
+    mn_lg: [],
+    ipr: !1,
+    loap: 1,
+    dcs: 0,
+    fpcf:{
+        "td": Math.floor(Math.random() * (80 - 10)) + 10,
+        "data":function (){
+            return '448809179;54595805;dis;,7;true;true;true;-480;true;24;24;true;false;-1'
+        },
+        "fpValstr":'448809179;54595805;dis;,7;true;true;true;-480;true;24;24;true;false;-1'
+    }
+    ,
+    ir: function () {
+        bmak["start_ts"] = Date["now"]() , bmak["kact"] = "", bmak["ke_cnt"] = 0, bmak["ke_vel"] = 0, bmak["mact"] = "", bmak["mme_cnt"] = 0, bmak["mduce_cnt"] = 0, bmak["me_vel"] = 0, bmak["pact"] = "", bmak["pme_cnt"] = 0, bmak["pduce_cnt"] = 0, bmak["pe_vel"] = 0, bmak["tact"] = "", bmak["tme_cnt"] = 0, bmak["tduce_cnt"] = 0, bmak["te_vel"] = 0, bmak["doact"] = "", bmak["doe_cnt"] = 0, bmak["doe_vel"] = 0, bmak["dmact"] = "", bmak["dme_cnt"] = 0, bmak["dme_vel"] = 0, bmak["vcact"] = "", bmak["vc_cnt"] = 0, bmak["aj_indx"] = 0, bmak["aj_ss"] = 0, bmak["aj_type"] = -1, bmak["aj_indx_doact"] = 0, bmak["aj_indx_dmact"] = 0, bmak["aj_indx_tact"] = 0, bmak["me_cnt"] = 0, bmak["pe_cnt"] = 0, bmak["te_cnt"] = 0, bmak["nav_perm"] = "", bmak["brv"] = 0, bmak["hbCalc"] = !1, bmak["fmh"] = "", bmak["fmz"] = "", bmak["ssh"] = "", bmak["wv"] = "", bmak["wr"] = "", bmak["weh"] = "", bmak["wl"] = 0, bmak["pdet"] = !0;
+    },
+    get_cf_date: function () {
+        return Date["now"]() ;
+    },
+    uar: function () {
+        return navigator["userAgent"];
+    },
+    gd: function () {
+        var t = bmak["uar"](),
+            a = "" + bmak["ab"](t),
+            e = bmak["start_ts"] / 2,
+            n = window["screen"]["availWidth"],
+            o =  window["screen"]["availHeight"],
+            m = window["screen"]["width"],
+            r = window["screen"]["height"],
+            i = window["innerHeight"],
+            c = window["innerWidth"],
+            b = window["outerWidth"];
+
+        bmak["z1"] = parseInt(bmak["start_ts"] / (bmak["y1"] * bmak["y1"]));
+        var d = Math["random"](),
+            s = parseInt(1000 * d / 2),
+            k = d + "";
+        return k = k["slice"](0, 11) + s, bmak["get_browser"](), bmak["bmisc"](), t + ",uaend," + 12147 + "," + bmak["psub"] + "," + bmak["lang"] + "," + bmak["prod"] + "," + bmak["plen"] + "," + bmak["pen"] + "," + bmak["wen"] + "," + bmak["den"] + "," + bmak["z1"] + "," + bmak["d3"] + "," + n + "," + o + "," + m + "," + r + "," + c + "," + i + "," + b + "," + bmak["bd"]() + "," + a + "," + k + "," + e + "," + bmak["brv"] + ",loc:" + bmak["loc"];
+    },
+    od: function (t, a) {
+        try {
+            t = String(t), a = String(a);
+            var e = [],
+                n = a["length"];
+
+            if (n > 0) {
+                for (var o = 0; o < t["length"]; o++) {
+                    var m = t["charCodeAt"](o),
+                        r = t["charAt"](o),
+                        i = a["charCodeAt"](o % n);
+                    m = bmak["rir"](m, 47, 57, i), m != t["charCodeAt"](o) && (r = String["fromCharCode"](m)), e["push"](r);
+                }
+
+                if (e["length"] > 0) return e["join"]("");
+            }
+        } catch (t) {}
+
+        return t;
+    },
+    get_browser: function () {
+        navigator["productSub"] && (bmak["psub"] = navigator["productSub"]), navigator["language"] && (bmak["lang"] = navigator["language"]), navigator["product"] && (bmak["prod"] = navigator["product"]), bmak["plen"] = 5;
+    },
+    bmisc: function () {
+        bmak["pen"] =  0, bmak["wen"] =  0, bmak["den"] =0;
+    },
+    bd: function () {
+        return ',cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:1,bat:1,x11:0,x12:1';
+    },
+    fas: function () {
+        // return Boolean(navigator["credentials"]) + (Boolean(navigator["appMinorVersion"]) << 1) + (Boolean(navigator["bluetooth"]) << 2) + (Boolean(navigator["storage"]) << 3) + (Boolean(Math["imul"]) << 4) + (Boolean(navigator["getGamepads"]) << 5) + (Boolean(navigator["getStorageUpdates"]) << 6) + (Boolean(navigator["hardwareConcurrency"]) << 7) + (Boolean(navigator["mediaDevices"]) << 8) + (Boolean(navigator["mozAlarms"]) << 9) + (Boolean(navigator["mozConnection"]) << 10) + (Boolean(navigator["mozIsLocallyAvailable"]) << 11) + (Boolean(navigator["mozPhoneNumberService"]) << 12) + (Boolean(navigator["msManipulationViewsEnabled"]) << 13) + (Boolean(navigator["permissions"]) << 14) + (Boolean(navigator["registerProtocolHandler"]) << 15) + (Boolean(navigator["requestMediaKeySystemAccess"]) << 16) + (Boolean(navigator["requestWakeLock"]) << 17) + (Boolean(navigator["sendBeacon"]) << 18) + (Boolean(navigator["serviceWorker"]) << 19) + (Boolean(navigator["storeWebWideTrackingException"]) << 20) + (Boolean(navigator["webkitGetGamepads"]) << 21) + (Boolean(navigator["webkitTemporaryStorage"]) << 22) + (Boolean(Number["parseInt"]) << 23) + (Boolean(Math["hypot"]) << 24);
+        return 30261693
+    },
+    getmr: function () {
+        bmak["mr"]= '20,20,40,20,20,40,40,20,20,20,20,20,20,100,'
+    },
+    sed: function () {
+        return '0,0,0,0,1,0,0'
+    },
+    x2: function () {
+        return Date.now()
+    },
+    np: function () {
+        bmak["nav_perm"]= '11321144241322243122'
+    },
+    cpa: function (t, a) {
+        try {
+            var e = !1;
+
+            if (1 == a && bmak["pme_cnt"] < bmak["pme_cnt_lmt"] || 1 != a && bmak["pduce_cnt"] < bmak["pduce_cnt_lmt"]) {
+                var n = t || window["event"];
+
+                if (n && "mouse" != n["pointerType"]) {
+                    e = !0;
+                    var o = -1,
+                        m = -1;
+                    n && n["pageX"] && n["pageY"] ? (o = Math["floor"](n["pageX"]), m = Math["floor"](n["pageY"])) : n && n["clientX"] && n["clientY"] && (o = Math["floor"](n["clientX"]), m = Math["floor"](n["clientY"]));
+                    var r = bmak["get_cf_date"]() - bmak["start_ts"],
+                        i = bmak["pe_cnt"] + "," + a + "," + r + "," + o + "," + m;
+                    void 0 !== n["isTrusted"] && !1 === n["isTrusted"] && (i += ",0"), bmak["pe_vel"] = bmak["pe_vel"] + bmak["pe_cnt"] + a + r + o + m, bmak["pact"] = bmak["pact"] + i + ";", bmak["ta"] += r, 1 == a ? bmak["pme_cnt"]++ : bmak["pduce_cnt"]++;
+                }
+            }
+
+            1 == a ? bmak["pme_cnt"]++ : bmak["pduce_cnt"]++, bmak["pe_cnt"]++, bmak["js_post"] && 3 == a && e && (bmak["aj_type"] = 2, bmak["bpd"](), bmak["pd"](!0), bmak["ce_js_post"] = 1);
+        } catch (t) {}
+    },
+    ab: function (t) {
+        if (null == t) return -1;
+
+        try {
+            for (var a = 0, e = 0; e < t["length"]; e++) {
+                var n = t["charCodeAt"](e);
+                n < 128 && (a += n);
+            }
+
+            return a;
+        } catch (t) {
+            return -2;
+        }
+    },
+    cal_dis: function (t) {
+        var a = t[0] - t[1],
+            e = t[2] - t[3],
+            n = t[4] - t[5],
+            o = Math["sqrt"](a * a + e * e + n * n);
+        return Math["floor"](o);
+    },
+    cc: function (t) {
+        var a = t % 4;
+        2 == a && (a = 3);
+        var e = 42 + a,
+            n = function (t, a) {
+                return 0;
+            };
+
+        if (42 == e) var n = function (t, a) {
+            return t * a;
+        };else if (43 == e) var n = function (t, a) {
+            return t + a;
+        };else var n = function (t, a) {
+            return t - a;
+        };
+        return n;
+    },
+    to: function () {
+        var t = Date.now() % 10000000;
+        bmak["d3"] = t;
+        for (var a = t, e = 3, n = 0; n < 5; n++) {
+            var o = parseInt(t / Math["pow"](10, n)) % 10,
+                m = o + 1;
+            a = bmak["cc"](o)(a, m);
+        }
+        bmak["o9"] = a * e;
+    },
+    jrs: function (t) {
+        for (var a = Math["floor"](1e5 * Math["random"]() + 1e4), e = String(t * a), n = 0, o = [], m = e["length"] >= 18; o["length"] < 6;) o["push"](parseInt(e["slice"](n, n + 2))), n = m ? n + 3 : n + 2;
+        return [a, bmak["cal_dis"](o)];
+    },
+    rir: function(t, a, e, n) {
+        return t > a && t <= e && (t += n % (e - a)) > e && (t = t - e + a),
+        t
+    },
+    fm: function () {
+        bmak["fmz"] = 1.25;
+    },
+    form_submit: function () {
+        try {
+            if (bmak["bpd"](), 0 == bmak["sdfn"]["length"]) {
+                if (document["getElementById"]("bm-telemetry") && (document["getElementById"]("bm-telemetry")["value"] = bmak["sensor_data"]), void 0 !== document["getElementsByName"]("bm-telemetry")) for (var t = document["getElementsByName"]("bm-telemetry"), a = 0; a < t["length"]; a++) t[a]["value"] = bmak["sensor_data"];
+            } else for (var a = 0; a < bmak["sdfn"]["length"]; a++) document["getElementById"](bmak["sdfn"][a]) && (document["getElementById"](bmak["sdfn"][a])["value"] = bmak["sensor_data"]);
+        } catch (t) {
+            bmak["sd_debug"](",s7:" + t + "," + bmak["sensor_data"]);
+        }
+    },
+    get_telemetry: function () {
+        return bmak["bpd"](), bmak["ir"](), bmak["sensor_data"];
+    },
+    getdurl: function () {
+        return document["URL"]
+    },
+    x1: function () {
+        return Math["floor"](16777216 * (1 + Math["random"]()))["toString"](36);
+    },
+    gck: function () {
+        var t = bmak["x1"]() + bmak["x1"]() + bmak["x1"]() + bmak["x1"]();
+        return bmak["set_cookie"](bmak["ckie"], t + "_" + bmak["ab"](t)), t;
+    },
+
+    get_cookie: function () {
+        var t = bmak["cookie_chk_read"](bmak["ckie"]);
+        return t;
+    },
+    cookie_chk_read: function (t) {
+        return document.abck_old_cookie;
+    },
+    build_pua: function () {
+        try {
+            navigator["userAgentData"]["getHighEntropyValues"](["architecture", "model", "platform", "platformVersion", "uaFullVersion", "bitness"])["then"](function (t) {
+                var a = [];
+                a["push"]("Mozilla/5.0 (");
+                var e = [];
+                e["push"](t["platform"], t["platformVersion"], t["architecture"], t["bitness"], t["model"]), a["push"](e["join"](";") + ")"), a["push"](" AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"), a["push"](t["uaFullVersion"]), navigator["userAgentData"]["mobile"] && a["push"](" Mobile"), a["push"](" Safari/537.36"), bmak["pua"] = a["join"]("");
+            });
+        } catch (t) {
+            bmak["pua"] = "";
+        }
+    },
+    isChrome: function () {
+       return true;
+    },
+    updatet: function () {
+        return bmak["get_cf_date"]() - bmak["start_ts"];
+    },
+    bpd: function () {
+        var t = 0;
+
+        try {
+            t = bmak["get_cf_date"]();
+            var a = bmak["updatet"](),
+                e = "3";
+            bmak["ckie"] && (e = bmak["get_cookie"]());
+            var n = bmak["gd"](),
+                o = "do_en",
+                m = "dm_en",
+                r = "t_en",
+                i = o + "," + m + "," + r,
+                c = "",
+                b = location.href,
+                d = bmak["aj_type"] + "," + bmak["aj_indx"];
+            //!bmak["fpcf"]["fpValCalculated"] && (0 == bmak["js_post"] || bmak["aj_indx"] > 0) && bmak["fpcf"]["fpVal"]();
+
+            var s = bmak["ke_vel"] + bmak["me_vel"] + bmak["doe_vel"] + bmak["dme_vel"] + bmak["te_vel"] + bmak["pe_vel"],
+                l = 'PiZtE',
+                u = bmak["jrs"](bmak["start_ts"]),
+                _ = bmak["get_cf_date"]() - bmak["start_ts"],
+                f = parseInt(bmak["d2"] / 6),
+                p = bmak["fas"](),
+                h = 0,
+                v = -1,
+                g = [bmak["ke_vel"] + 1, bmak["me_vel"] + 32, bmak["te_vel"] + 32, bmak["doe_vel"], bmak["dme_vel"], bmak["pe_vel"], s, a, bmak["init_time"], bmak["start_ts"], bmak["fpcf"]["td"], bmak["d2"], bmak["ke_cnt"], bmak["me_cnt"], f, bmak["pe_cnt"], bmak["te_cnt"], _, bmak["ta"], bmak["n_ck"], e, bmak["ab"](e), bmak["fpcf"]["rVal"], bmak["fpcf"]["rCFP"], p, l, u[0], u[1], h, v],
+                w = g["join"](","),
+                y = "" + bmak["ab"](bmak["fpcf"]["fpValstr"]);
+            bmak["fmh"]='b70ca7d25d21c53971815e2b7f03804af1fbd2ad0f6c3155b9cd8b617e7b2627'
+            bmak["ssh"]='e6a6b62042f2333998b8357baa6fe8b776aaad3c0b1692df334f2afaf3954baf'
+            bmak["wv"]='Google Inc. (Intel)'
+            bmak["wr"]='ANGLE (Intel, Intel(R) Iris(R) Xe Graphics Direct3D11 vs_5_0 ps_5_0, D3D11)'
+            bmak["weh"]='95f5b71fe531f867faa814bdd4050dd8057206d53ecec1163523560525884870'
+            bmak["wl"]=33
+            !bmak["hbCalc"] && (0 == bmak["js_post"] || bmak["aj_indx"] > 0) && (bmak["fm"](), bmak["hbCalc"] = !0);
+            var E = "";
+            bmak["hbCalc"] && (E = bmak["fmh"] + "," + bmak["fmz"] + "," + bmak["ssh"] + "," + bmak["wv"] + "," + bmak["wr"] + "," + bmak["weh"] + "," + bmak["wl"]);
+
+            var S = bmak["sed"](),
+                C = [],
+                A = "",
+                B = "",
+                x = "";
+
+            if (void 0 !== C[1]) {
+                var M = C[1];
+                void 0 !== bmak["mn_r"][M] && (A = bmak["mn_r"][M]);
+            }
+
+            if (void 0 !== C[2]) {
+                var j = C[2];
+                void 0 !== bmak["mn_r"][j] && (B = bmak["mn_r"][j]);
+            }
+
+            if (void 0 !== C[3]) {
+                var L = C[3];
+                void 0 !== bmak["mn_r"][L] && (x = bmak["mn_r"][L]);
+            }
+            debugger
+            true && bmak["isChrome"]() && bmak["build_pua"](), bmak["sensor_data"] = bmak["ver"] + "-1,2,-94,-100," + n + "-1,2,-94,-131," + bmak["pua"] + "-1,2,-94,-101," + i + "-1,2,-94,-105," + bmak["informinfo"] + "-1,2,-94,-102," + c + "-1,2,-94,-108," + bmak["kact"] + "-1,2,-94,-110," + bmak["mact"] + "-1,2,-94,-117," + bmak["tact"] + "-1,2,-94,-111," + bmak["doact"] + "-1,2,-94,-109," + bmak["dmact"] + "-1,2,-94,-114," + bmak["pact"] + "-1,2,-94,-103," + bmak["vcact"] + "-1,2,-94,-112," + b + "-1,2,-94,-115," + w + "-1,2,-94,-106," + d, bmak["sensor_data"] = bmak["sensor_data"] + "-1,2,-94,-119," + bmak["mr"] + "-1,2,-94,-122," + S + "-1,2,-94,-123," + A + "-1,2,-94,-124," + B + "-1,2,-94,-126," + x + "-1,2,-94,-127," + bmak["nav_perm"];
+            var P = 24 ^ bmak["ab"](bmak["sensor_data"]);
+            bmak["sensor_data"] = bmak["sensor_data"] + "-1,2,-94,-70," + bmak["fpcf"]["fpValstr"] + "-1,2,-94,-80," + y + "-1,2,-94,-116," + bmak["o9"] + "-1,2,-94,-118," + P + "-1,2,-94,-129," + E + "-1,2,-94,-121,";
+        } catch (t) {
+            var T = "";
+            console.log("错误分支")
+
+            t["stack"] && "string" == typeof t["stack"] ? T = t["stack"]["replace"](/\"/g, "\\'") : "string" == typeof t && (T = t["replace"](/\"/g, "\\'")), T = T["slice"](0, 1e3),  bmak["sensor_data"] = bmak["ver"] + "-1,2,-94,-100," + bmak["uar"]() + "-1,2,-94,-120," + T;
+
+        }
+
+        try {
+            var F = bmak["od"](bmak["cs"], bmak["api_public_key"])["slice"](0, 16),
+                D = Math["floor"](bmak["get_cf_date"]() / 36e5),
+                N = bmak["get_cf_date"](),
+                R = F + bmak["od"](D, F) + bmak["sensor_data"];
+            bmak["sensor_data"] = R + ";" + (bmak["get_cf_date"]() - t) + ";" + bmak["tst"] + ";" + (bmak["get_cf_date"]() - N);
+        } catch (t) {
+            console.log("error")
+        }
+
+
+    }
+};
+
+bmak["ir"](), bmak["t_tst"] = bmak["get_cf_date"](), bmak["to"](), bmak["tst"] = bmak["get_cf_date"]() - bmak["t_tst"], bmak["disFpCalOnTimeout"]
+bmak["calc_fp"]
+
+function get_sensor_data(){
+    bmak.bpd();
+    return bmak.sensor_data;
+}
+
